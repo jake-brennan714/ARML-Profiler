5df7b009f82c40064866ffbe77a26920ce31bbf5
jake-brennan714 <jake.brennan714@gmail.com>
2024-04-24 16:49:08 -0400

    Fixed some stuff, added pageview controller

---
+++ src/main/java/net/jakebrennan/web/rest/PageviewController.java
@@ -0,0 +1,13 @@
+package net.jakebrennan.web.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageviewController {
+    //    @GetMapping("/")
+    //    public String index() {
+    //        return "index";
+    //    }
+
+}
